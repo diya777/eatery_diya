4db3e9e6a34b9643cd5bf1c6ae82d17bbf4c6046
lawrence <svzef@outlook.com>
2018-04-24 20:41:06 +0800

    first commit

---
+++ order/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home_page(request):
+
+	if request.method == 'POST':
+  	  Item = request.POST.get('item')
+  	  address = request.POST.get( "address")
+  	  return render(request, 'order/home_page.html',{'item':item,'address':address})
+
+	return render(request, 'order/home_page.html' )
